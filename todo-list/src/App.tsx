5c5a4bca4b2994d047350873a395448eb8905188
Nathan Storm <nathanstorm95@csu.fullerton.edu>
2024-05-14 23:36:09 -0700

    FeatureL: Added background color

--- todo-list/src/App.tsx
+++ todo-list/src/App.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 function App() {
   return (
-    <div>
+    <div style={{ backgroundColor: "#175e5d", padding: "10%" }}>
       <Paper
         elevation={3}
         square={false}
@@ -12,7 +12,7 @@ function App() {
           padding: "15px",
           minHeight: "75vh",
           borderRadius: "30px",
-          margin: "10%",
+          // margin: "10%",
         }}
       ></Paper>
     </div>
