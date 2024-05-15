ddfef33bb671770678066ce9c5b081ba37ff36d4
Nathan Storm <nathanstorm95@csu.fullerton.edu>
2024-05-14 23:28:34 -0700

    Feature: Finished the base container view

--- todo-list/src/App.tsx
+++ todo-list/src/App.tsx
@@ -1,7 +1,22 @@
+import { Paper } from "@mui/material";
 import React from "react";
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div>
+      <Paper
+        elevation={3}
+        square={false}
+        sx={{
+          width: "75vw",
+          padding: "15px",
+          minHeight: "75vh",
+          borderRadius: "30px",
+          margin: "10%",
+        }}
+      ></Paper>
+    </div>
+  );
 }
 
 export default App;
